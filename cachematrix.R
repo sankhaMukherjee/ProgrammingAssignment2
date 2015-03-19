e41d21922fca070d3f86b266a11005b70dc41ea9
Sankha Subhra Mukherjee <sankha.mukherjee@gmail.com>
2015-03-19 22:38:39 +0800

    with comments added

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,31 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
+##############################################################
 ## Write a short comment describing this function
+## This function returns a list containing four 
+## functions, which have the matrix `x` and the inverse
+## of the matrix `inv` as floating variables to all 
+## these functions. This function is called with the
+## required value of `x` set (default value is an empty
+## matrix). 
+
+## The functions `getValue()` and `getInv()` just return
+##  the values `x` and `inv` respectively. 
+
+## The functions `setValue()` and `setInv()` takes an input
+##  value and just makes the floating values in their parent 
+##  envirinment equal to the one supplied
+##
+## --------------------------
+## Example Execution:
+#   for an available matrix `c`
+# > c
+# [,1]  [,2]
+# [1,]  1.00 -0.25
+# [2,] -0.25  1.00
+# > xx = makeCacheMatrix(c)
+##############################################################
 
 makeCacheMatrix <- function(x = matrix()) {
     inv <- NULL
@@ -25,9 +49,31 @@ makeCacheMatrix <- function(x = matrix()) {
         )
 }
 
-
+###########################################################
 ## Write a short comment describing this function
-
+# 
+## cacheSolve() 
+##  It first checks to see if a value is present in the 
+##    `inv` variable in the environment of the `xx` 
+##    class. If the `inv` is `NULL` then it is calculated,
+##    and the value of `inv` is set to this calculated 
+##    value, otherwise it doesnt do anything. The return value 
+##    is simply the value of `inv`
+############################################################
+# > cacheSolve(xx)
+# [,1]      [,2]
+# [1,] 1.0666667 0.2666667
+# [2,] 0.2666667 1.0666667
+# > cacheSolve(xx)
+# this is catched
+# [,1]      [,2]
+# [1,] 1.0666667 0.2666667
+# [2,] 0.2666667 1.0666667
+# > c
+# [,1]  [,2]
+# [1,]  1.00 -0.25
+# [2,] -0.25  1.00
+#######################################################
 cacheSolve <- function(x, ...) {
     
     ## Return a matrix that is the inverse of 'x'
